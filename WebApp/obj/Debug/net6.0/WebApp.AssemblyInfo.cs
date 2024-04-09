c815beb9132bb233fc36e7607e770706092065ec
DannyCandy <dankbzzz01@gmail.com>
2024-04-09 21:45:09 +0700

    commit2

--- WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196570b67882d34e130710078eb36a5b19ca122b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7115ddedc3442f8e1a01c4a1ac81443731ae650d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
